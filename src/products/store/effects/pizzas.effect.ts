ceeb6ece06ce5b5c7f4d50a929dccc49c85ba5ac
samuel <sam.teboul@gmail.com>
2020-01-29 18:19:35 +0200

    Multiple Actions in Effects, Router Actions

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -4,6 +4,7 @@ import { of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import * as fromServices from '../../services';
 import * as pizzaActions from '../actions/pizzas.action';
+import * as fromRoot from '../../../app/store';
 
 @Injectable()
 export class PizzasEffects {
@@ -32,6 +33,13 @@ export class PizzasEffects {
   );
 
   @Effect()
+  createPizzaSuccess$ = this.actions$.pipe(
+    ofType(pizzaActions.CREATE_PIZZA_SUCCESS),
+    map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+    map(pizza => new fromRoot.Go({ path: ['/products', pizza.id] }))
+  );
+
+  @Effect()
   updatePizza$ = this.actions$.pipe(
     ofType(pizzaActions.UPDATE_PIZZA),
     map((action: pizzaActions.UpdatePizza) => action.payload),
@@ -49,5 +57,11 @@ export class PizzasEffects {
       map(() => new pizzaActions.DeletePizzaSuccess(pizza)),
       catchError(error => of(new pizzaActions.DeletePizzaFail(error)))
     ))
+  );
+
+  @Effect()
+  handlePizzaSuccess$ = this.actions$.pipe(
+    ofType(pizzaActions.UPDATE_PIZZA_SUCCESS, pizzaActions.DELETE_PIZZA_SUCCESS),
+    map(() => new fromRoot.Go({ path: ['/products'] }))
   );
 }
